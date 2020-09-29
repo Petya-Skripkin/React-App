8adb28f7aac2c5134c61822696646cd18c615371
Petya-Skripkin <sk.petya02@mail.ru>
2020-09-29 13:31:53 +0600

    master settings

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import * as Component from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Component.Header/>
+    </div>
+  );
+}
+
+export default App;
